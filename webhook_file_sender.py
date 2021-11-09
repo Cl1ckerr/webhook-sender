56cf3bfb11cd7f8eaae99780b805882f38b4d920
Cl1cker <75802182+Cl1ckerr@users.noreply.github.com>
2021-11-08 22:07:14 -0400

    Delete webhook_file_sender.py

--- webhook_file_sender.py
+++
@@ -1,8 +0,0 @@
-from discord_webhook import DiscordWebhook, DiscordEmbed
-
-webhook = DiscordWebhook(url='https://discord.com/api/webhooks/907427309055787028/fXhgfTYXLPHVWz99QbPGVjU9eNIiNoyMvb3qs04UbU_Sq4Uzu5xWiKEqJlLsHZVy1R24', content='Esta Va Para <@652254918622707778>')
-
-with open("C:/Users/jrodr/Downloads/265f02a95ab74ff49bc21d2b8ded177a.m4a", "rb") as f:
-    webhook.add_file(file=f.read(), filename='Esta Va Para Sudo.mp3')
-
-response = webhook.execute()
